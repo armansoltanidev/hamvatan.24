1fae8773667a7fdaa92ccf0c2703516197122190
armandev <armansoltanidev@gmail.com>
2024-03-18 01:43:51 +0330

    (fix): fixloading ui

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function loading() {
   return (
     <div className="absolute inset-0 backdrop-blur-md flex items-center justify-center">
-      <div className="loader"></div>
+      <div className="loader sr-only">loading</div>
     </div>
   );
 }
