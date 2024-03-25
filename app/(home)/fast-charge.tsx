7bc1e6edd39c90a1b8ad86529e37b28bef978442
armandev <armansoltanidev@gmail.com>
2024-03-26 01:28:58 +0330

    (style): add fast charge section, fix responsice in main landing

---
+++ app/(home)/fast-charge.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import TypographyH1, {
+  TypographyH2,
+  TypographyH3,
+  TypographyH4,
+} from "@/components/ui/typography";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export default function FastCharge() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <>
+      <Form {...form}>
+        <form
+          id="fast-charge"
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col md:flex-row gap-3 justify-between h-full p-6"
+        >
+          <div className="flex-1 bg-white rounded-2xl p-6 flex flex-col gap-y-5 justify-around">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-lg font-semibold">
+                    شماره مقصــد
+                  </FormLabel>
+                  <FormControl>
+                    <Input className="py-8 px-6 text-xl" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    شماره را بدون صفر اول وارد کنید
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-lg font-semibold">
+                    مبلغ شــارژ
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="py-8 px-6 text-xl text-center"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    حداقل مبلغ شــارژ ۵۰ افغانی میباشد
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full py-6" variant="cta">
+              محاسبه کن
+            </Button>
+          </div>
+          <div className="flex-1 bg-white rounded-2xl p-6 flex flex-col items-center justify-around">
+            <div className="flex flex-col gap-y-2">
+              <TypographyH4>شماره مقصد: ۹۳۷۸۴۵۱۲۴۴۷۴+</TypographyH4>
+              <TypographyH4>اوپــراتور مقصد: اتصالات</TypographyH4>
+            </div>
+
+            <div>
+              <TypographyH2 className="text-emerald-600">
+                <span className="text-base">مبلغ قابــل پرداخت:</span>
+                <span className="mx-4">۱,۴۵۱,۴۸۴</span>
+              </TypographyH2>
+              <div>
+                <ul className="list-disc text-sm font-normal text-emerald-800">
+                  <li>هزینه شارژ: ۱,۲۹,۴۱۵</li>
+                  <li>مالیات بر ارزش افزوده : ۹۸,۴۵۴</li>
+                  <li>هزینه خدمات: ۲۹,۴۱۵</li>
+                  <li>تخفیف: ۰</li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex flex-col gap-y-3">
+              <div className="flex items-center gap-x-2">
+                <Checkbox id="terms" />
+                <label
+                  htmlFor="terms"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  میخواهم شماره را خود را برای دریافت پیگیری های آتی وارد کنم
+                </label>
+              </div>
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="شماره تلفن جهت پیگیری های آتی"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          <div className="flex-1 bg-white rounded-2xl flex flex-col justify-between p-6">
+            <TypographyH4>انتخاب درگاه پرداخت</TypographyH4>
+            <Button
+              type="submit"
+              className="w-full text-2xl"
+              variant="primary"
+              size="x2l"
+            >
+              برو به درگاه
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,40 @@
 import { Button } from "@/components/ui/button";
 import CardAnimate from "@/components/ui/card-date/card-animated";
 import Logo from "@/components/ui/logo/logo";
-import TypographyH1, { TypographyP } from "@/components/ui/typography";
+import TypographyH1, {
+  TypographyH4,
+  TypographyP,
+} from "@/components/ui/typography";
 import Image from "next/image";
 import Link from "next/link";
+import FastCharge from "./(home)/fast-charge";
 
 export default function Page() {
   return (
-    <div className="h-screen mx-auto max-w-[1440px]">
+    <div className="h-screen mx-auto max-w-[1440px] px-4">
       <div className="flex items-center justify-between py-8">
         <Logo />
         <nav>
-          <ul className="flex items-center gap-x-10 text-muted-foreground">
+          <ul className="hidden md:flex items-center gap-x-10 text-muted-foreground">
             <Link href="/">صفحه اصلی</Link>
-            <Link href="/">محصولات</Link>
+            <Link href="/panel/hybrid">محصولات</Link>
             <Link href="/">شارژ سریع</Link>
             <Link href="/">بلاگ</Link>
             <Link href="/">پشتیبانی</Link>
           </ul>
         </nav>
         <div>
-          <Button size="xl" variant="cta">
-            ورود | ثبت نام
-          </Button>
+          <Link href="/login">
+            <Button size="xl" variant="cta">
+              ورود | ثبت نام
+            </Button>
+          </Link>
         </div>
       </div>
-      <main className="h-4/6 mt-8 rounded-3xl w-full bg-primary grid grid-cols-6">
-        <div className="col-span-2"></div>
-        <div className="col-span-4 py-16 px-6">
-          <h1 className="bg-gradient-to-tl text-7xl font-extrabold leading-normal tracking-normal from-slate-500 via-slate-300 to-slate-100 text-transparent bg-clip-text py-6">
+      <main className="h-auto mt-8 rounded-3xl w-full bg-primary grid grid-cols-6 py-4 px-6">
+        <div className="hidden md:block col-span-2"></div>
+        <div className="col-span-6 md:col-span-4 px-3 py-16 md:px-6">
+          <h1 className="bg-gradient-to-tr text-4xl md:text-7xl font-extrabold leading-normal tracking-normal from-slate-500 via-slate-300 to-slate-100 text-transparent bg-clip-text py-6">
             سریع ترین روش خرید شارژ افغانستان
           </h1>
           <TypographyP className="text-muted-foreground text-lg">
@@ -37,18 +43,53 @@ export default function Page() {
             با ثبت نام در پنل به راحتی و سریع تراکنش هاتون رو انجام و از مزایای
             پنــل استفاده کنید 😉
           </TypographyP>
-          <div className="flex gap-x-2 my-6">
+          <div className="flex flex-col items-center md:items-start md:flex-row gap-2 my-6">
             <CardAnimate />
-            <Button
-              className="bg-primary-foreground text-primary text-xl font-normal"
-              size="x2l"
-              variant="cta"
-            >
-              شارژ سریع و ارزان
-            </Button>
+            <Link href="#fast-charge">
+              <Button
+                className="bg-primary-foreground text-primary text-xl font-normal"
+                size="x2l"
+                variant="cta"
+              >
+                شارژ سریع و ارزان
+              </Button>
+            </Link>
           </div>
+          <section className="flex flex-col md:flex-row gap-y-6 items-center justify-between mt-12">
+            <div className="flex items-center gap-x-5">
+              <Image width={62} height={62} alt="enamad" src="/enamad.svg" />
+              <div>
+                <TypographyP className="text-primary-foreground font-medium  leading-tight">
+                  معتبر ترین درگاه خرید شارژ افغانســتان
+                </TypographyP>
+                <TypographyP className="text-muted-foreground">
+                  دارای نماد اعتماد الکترونیک از وزارت صنعت، معدن و تجارت
+                </TypographyP>
+              </div>
+            </div>
+            <div>
+              <TypographyP className="text-muted-foreground text-base">
+                مورد اعتماد بیش از
+              </TypographyP>
+              <TypographyH4 className="bg-gradient-to-tl text-5xl font-extrabold leading-tight tracking-normal from-slate-500 via-slate-300 to-slate-100 text-transparent bg-clip-text">
+                10K+
+              </TypographyH4>
+              <div className="flex items-center gap-x-2">
+                <span className="relative flex items-center justify-center h-3 w-3">
+                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                  <span className="relative inline-flex rounded-full h-2 w-2 bg-sky-500"></span>
+                </span>
+                <TypographyP className="text-muted-foreground text-sm">
+                  نماینده فعال در سراسر ایران
+                </TypographyP>
+              </div>
+            </div>
+          </section>
         </div>
       </main>
+      <div className="bg-secondary rounded-3xl h-auto my-16">
+        <FastCharge />
+      </div>
     </div>
   );
 }
